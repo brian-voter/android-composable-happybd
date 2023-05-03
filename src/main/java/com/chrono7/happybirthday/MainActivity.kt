c0e92507fe32280f636acf6407d2857fd1ee06aa
bvoter <bvoter97@gmail.com>
2023-05-03 00:30:22 -0400

    add bg image and accessibility features

--- src/main/java/com/chrono7/happybirthday/MainActivity.kt
+++ src/main/java/com/chrono7/happybirthday/MainActivity.kt
@@ -3,7 +3,10 @@ package com.chrono7.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,7 +39,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText("Happy birthday, viewer of my github!", "Brian")
+                    GreetingImage(
+                        stringResource(R.string.happy_birthday_text),
+                        stringResource(R.string.signature_text)
+                    )
                 }
             }
         }
@@ -39,8 +52,49 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Column(verticalArrangement = Arrangement.Center, modifier = modifier) {
-        Text(text = message, fontSize = 70.sp, lineHeight = 80.sp, textAlign = TextAlign.Left)
-        Text(text = from, fontSize = 36.sp, modifier = Modifier.padding(16.dp).align(Alignment.End))
+        Text(
+            text = message,
+            fontSize = 70.sp,
+            lineHeight = 80.sp,
+            textAlign = TextAlign.Left,
+            style = TextStyle(
+                color = Color.White,
+                shadow = Shadow(Color.Black, Offset(-4F, 4F), 10F)
+            )
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            style = TextStyle(
+                color = Color.White,
+                shadow = Shadow(Color.Black, Offset(-4F, 4F))
+            ),
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.End)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.blackhole)
+
+    Box(contentAlignment = Alignment.Center, modifier = modifier.background(Color.Black)){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            alignment = Alignment.Center
+//            modifier = Modifier.rotate(90F)
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
 
@@ -48,6 +102,9 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText("Happy birthday, viewer of my github!", "Brian")
+        GreetingImage(
+            stringResource(R.string.happy_birthday_text),
+            stringResource(R.string.signature_text)
+        )
     }
 }
